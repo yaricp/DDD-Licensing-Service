947122d01e6a7b058f6dbb4cd9a47603345759a7
Yaroslav Pisarev <yaricp@gmail.com>
2025-09-15 18:32:10 +0200

    scripts/build_images.sh was fixed

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -e
+
+mkdir -p src/telegram_client/protobuf_types/
+mkdir -p src/email_client/protobuf_types/
+mkdir -p src/backend/licensing_service/infra/protobuf_types/
+
 cp -R src/protobuf_types/* src/telegram_client/protobuf_types/
 cp -R src/protobuf_types/* src/email_client/protobuf_types/
 cp -R src/protobuf_types/* src/backend/licensing_service/infra/protobuf_types/
